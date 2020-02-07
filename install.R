b1d1bcd462f3b47ef459ae4338e5efc114e95ff5
muschellij2 <muschellij2@gmail.com>
2020-02-07 15:42:16 -0500

    fixing warnings

--- install.R
+++ install.R
@@ -1,3 +1,4 @@
+Sys.setenv(R_REMOTES_NO_ERRORS_FROM_WARNINGS = TRUE)
 install.packages(c("shiny", "shinyjs", "shinydashboard", 
                    "remotes", "dplyr", "googledrive"))
 remotes::install_github("muschellij2/didactr")
